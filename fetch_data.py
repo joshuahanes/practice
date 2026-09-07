49848cdf3f9057e6833cc3dcdafaab92b524d2a3
joshuahanes <36218466+joshuahanes@users.noreply.github.com>
2026-09-07 22:47:12 +0000

    Fetch data

--- fetch_data.py
+++ fetch_data.py
@@ -7,5 +7,14 @@ response=requests.get(url,
                       "longitude":-77.46,
                       "current":"temperature_2m"})
 data=response.json()
-print(data.keys())
-print(data["current"])
+#print(data.keys())
+#print(data["current"]["temperature_2m"])
+
+url_aq = "https://air-quality-api.open-meteo.com/v1/air-quality"
+aq_response=requests.get(url_aq,
+                      params={"latitude":37.55,
+                      "longitude":-77.46,
+                      "current":"pm2_5"})
+air_quality=aq_response.json()
+#print(air_quality.keys())
+print(air_quality["current"]["pm2_5"])
--- fetch_data_fun.py
+++ fetch_data_fun.py
@@ -1,29 +1,37 @@
 import requests
+
 def fetch_data(lat=37.55,lon=-77.46):
     """This function takes a lat long and returns temp and air qual"""
 
-    url1 = "https://"
-    url2 = "https://"
-
+    url1 = "https://api.open-meteo.com/v1/forecast"
+    url2 = "https://air-quality-api.open-meteo.com/v1/air-quality"
 
     response=requests.get(
         url1,
         params={
             "latitude":lat,
-            "longitude":long,
+            "longitude":lon,
             "current":"temperature_2m"
 
         })
     data=response.json()
     temperature=data["current"]["temperature_2m"]
 
-    response=requests.get(
-        url1,
+    aq_response=requests.get(
+        url2,
         params={
             "latitude":lat,
-            "longitude":long,
-            "current":"temperature_2m"
+            "longitude":lon,
+            "current":"pm2_5"
     
-        })
-    data=response.json()
-    temperature=data["current"]["temperature_2m"]
+            })
+    air_quality=aq_response.json()
+    pm2_5=air_quality["current"]["pm2_5"]
+
+    return {
+        "temperature":temperature,
+        "pm2_5":pm2_5,"lat":lat
+    }
+
+if __name__=="__main__":
+    print(fetch_data())
